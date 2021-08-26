9a52a05f2dfd52a76470493de03263d8e2651739
Jeff Spear <jspearus@gmail.com>
2021-08-25 19:49:07 -0500

    Update

--- website/auth.py
+++ website/auth.py
@@ -10,10 +10,10 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('email')
+        firstName = request.form.get('firstName')
         password = request.form.get('password')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(first_name=firstName).first()
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
@@ -22,7 +22,7 @@ def login():
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('name does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
 
@@ -33,19 +33,21 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
+@auth.route('/scorecard')
+@login_required
+def scorecard():
+    return render_template("scorecard.html", user=current_user)
+
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(first_name=first_name).first()
         if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 5 characters', category='error')
+            flash('Name already exists', category='error')
         elif len(first_name) < 2:
             flash('firstname must be greater than 2 characters', category='error')
         elif password1 != password2:
@@ -54,7 +56,7 @@ def sign_up():
             flash('Password must be greater than 5 characters', category='error')
         else:
             # add user to database
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
